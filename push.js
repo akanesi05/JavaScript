6e9c0bc2cc5c80283bb0a6fdb478a0cb204e8fd3
mitsuiakane <akane.sumitomo@gmail.com>
2024-10-09 14:02:42 +0900

    forEac

--- push.js
+++ push.js
@@ -6,3 +6,15 @@
 const numbers = [1, 2, 3]
 const newLength = numbers.unshift(10) // newLengthは4
 console.log(numbers) // [10, 1, 2, 3]
+
+//shiftメソッドを呼び出した配列の先頭の要素を取り出します
+//forEachメソッドは、配列の各要素に対して順番に処理を行います。
+numbers.forEach((number, index) => {
+    console.log(index)
+  })
+
+const prefectures = ['愛知', '福岡', '沖縄']
+prefectures.forEach((prefecture) => {
+  console.log(prefecture)
+  
+})
