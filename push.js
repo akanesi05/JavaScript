bb8af0702d233607deb0522e64e19f7676727f4b
mitsuiakane <akane.sumitomo@gmail.com>
2024-10-15 23:13:43 +0900

    found

--- push.js
+++ push.js
@@ -32,5 +32,20 @@ const greet = () => {
   
   runTwice(greet) 
 
-const sampleUrl = 'https://jsonplaceholder.typicode.com/users'
-const sample = fetch(sampleUrl) // ユーザーの一覧情報を含むデータを取得
+  const getUsersUrl = 'https://jsonplaceholder.typicode.com/users'
+  const response = fetch(getUsersUrl)
+  console.log(response)
+
+  const prices = [1000, 2000, 3000, 4000]
+const found = prices.find((price) => price >= 2500)
+console.log(found) // 3000
+const array = [4, 9, 13, 20, 25]
+console.log(array.find((value) => value < 5))
+console.log([1000, 2000, 3000, 4000].indexOf(1000)) // 0
+const tastes = ['醤油', '塩', '豚骨', '味噌']
+console.log(tastes.includes('塩'))
+const greeting2 = ['おはよう', 'こんにちは', 'ありがとう']
+const includesArray = (array) => {
+  return array.includes('ありがとう')
+}
+console.log(includesArray(greeting2))
