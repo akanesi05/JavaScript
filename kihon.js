ff527bac3e98bab0780c2e3aa48a5d59bd111641
mitsuiakane <akane.sumitomo@gmail.com>
2024-11-01 23:57:38 +0900

    アロー関数

---
+++ kihon.js
@@ -0,0 +1,18 @@
+const arrowFunc = function (a, b) {
+    return a + b;
+  }
+
+  const arrowFunc1 =(a , b)=>a + b
+
+  //従来の関数の記法
+function oldFunc(value) {
+    return value
+  }
+
+
+  console.log(oldFunc('従来の記法です'))
+///基本計const newFunc = () => {}
+  const newFunc = (value) => {
+    return value
+  }
+  console.log(newFunc('新しい記法です'))
