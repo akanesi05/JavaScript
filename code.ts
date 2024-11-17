8ecad94d13a94981f46f2aa74868d7cf2f4fcce3
mitsuiakane <akane.sumitomo@gmail.com>
2024-11-17 23:38:27 +0900

    基本

--- code.ts
+++ code.ts
@@ -5,3 +5,17 @@ const arraySample: string[] = ['りんご', 'みかん', 'バナナ']
 // 問題3. Array<型名>を使って型を指定
 
 const arraySample2: Array<string> = ['りんご', 'みかん', 'バナナ']
+
+//TypeScript基本的な型
+//boolean
+let bool: boolean = true;
+
+//number数字
+let num: number = 0;
+
+//string文字
+let str: string = "A";
+
+//Array 配列
+let arr1: Array<number> = [0, 1.2];
+let arr2: number[]=[0,1,2]
